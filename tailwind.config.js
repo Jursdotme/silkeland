f74003edef624325486281f99523edc47a3717b7
Rasmus Jürs <rasmus@jurs.me>
2021-05-09 16:47:26 +0200

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,18 +32,7 @@ module.exports = {
       colors: {
         white: '#ffffff',
         black: '#000000',
-        bgreen: {
-          50: '#edf3df',
-          100: '#d8e5ba',
-          200: '#c3d795',
-          300: '#aec96f',
-          400: '#99bb4a',
-          500: '#7e9e3e',
-          600: '#648133',
-          700: '#4c6327',
-          800: '#34461c',
-          900: '#1e2910',
-        },
+        bgreen: colors.rose,
         bsand: {
           50: '#f7f6f4',
           100: '#f9f7f6',
